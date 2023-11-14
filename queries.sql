4e93573719af53504397dcad852c3a177eeb840f
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2023-11-14 00:51:41 -0600

    Part 1 and 2 pushed

--- queries.sql
+++ queries.sql
@@ -1,6 +1,8 @@
 --Part 1
+--SELECT * FROM employer
 
 --Part 2
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    public Employer() {
+    }
+
+    @NotBlank
+    @Size(min = 3, max = 63)
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
